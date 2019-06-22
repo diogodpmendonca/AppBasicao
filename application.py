6f87c3e2da71bd638e57827d833fe00d1b711a0d
diogodpmendonca <diogodpmendonca@gmail.com>
2019-06-22 17:01:43 +0000

    correção application

--- application.py
+++ application.py
@@ -1,3 +1,8 @@
 from flask import Flask, render_template
+
 application = Flask(__name__)
-@application.route(‘/’) def main(): return render_template(‘index.html’)
+
+@application.route('/')
+
+def main():
+    return render_template('index.html')
